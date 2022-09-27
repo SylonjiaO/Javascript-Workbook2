4988df3b6887f457db746f7a67c2f3c04122f315
Sylonjia Odion <Sodion@atl.yearup.org>
2022-09-27 17:35:51 -0400

    circle area generating random whole numbers between 1-20

---
+++ WB2-Module2/Mathscripts/area_of_circle.js
@@ -0,0 +1,8 @@
+
+// calculations
+var c = Math.floor(Math.random() * (20 - 1+1)) + 1;
+var radius = c/2*Math.PI;
+var circleArea = Math.PI * radius**2;
+
+//output
+console.log("If c is " + c + " The area of the circle is " + circleArea);
