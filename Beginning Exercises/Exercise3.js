34eb96b81ae13b5cfc1aa079733aab877cb5b5fa
Sylonjia Odion <Sodion@atl.yearup.org>
2022-09-27 14:33:57 -0400

    added first exercises

---
+++ Beginning Exercises/Exercise3.js
@@ -0,0 +1,8 @@
+var FirstName, LastName,CityName,StateName;
+
+FirstName = "Sylonjia";
+LastName = "Odion";
+CityName = "Atlanta";
+StateName = "Georgia";
+
+console.log(FirstName, LastName, CityName, StateName);
