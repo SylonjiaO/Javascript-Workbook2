1362012fce205b70f5c35e4630076dd4cd0909dc
Sylonjia Odion <Sodion@atl.yearup.org>
2022-09-27 16:37:42 -0400

    tip amounts

---
+++ WB2-Module2/Mathscripts/tip_amount.js
@@ -0,0 +1,12 @@
+
+var bill = 170;
+var tipPercentage = .20;
+
+// calculations
+var tip = bill * tipPercentage;
+
+var totalBill = tip + bill;
+
+//output
+console.log("The tip on a $" + bill + " food bill is $" + tip);
+console.log("The total bill is $" + totalBill);
