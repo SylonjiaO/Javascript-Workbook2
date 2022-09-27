f73ef173a1da18cd3bcd1b36cb7998dc99dd1a41
Sylonjia Odion <Sodion@atl.yearup.org>
2022-09-27 18:36:49 -0400

    rule of 72

--- WB2-Module2/Mathscripts/rule_of_72.js
+++ WB2-Module2/Mathscripts/rule_of_72.js
@@ -1,8 +1,11 @@
-var length = 170;
-var width = 100;
-
+var currentSavings = Math.random(); 
 // calculations
-var rectArea = length * width;
+var ir= Math.random() * (5 - 0) + 0;
+var t = 72/ir;
+
+var savings = currentSavings * 2;
+
+
 
 //output
-console.log("The area of the rectangle is " + rectArea);
+console.log("At a " + ir.toFixed(1) + "% interest rate, your savings account of $" + currentSavings.toFixed(2) + " will be worth $" + savings.toFixed(2) + " in " + t.toFixed(1) + " years");
